733d7f8e6f243aef166466b277e8b32f8171c203
Clarice Bouwer <clarice@simply.co.za>
2022-11-03 06:25:35 +0400

    Create routes with react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import HomePage from './pages/Home';
+import RegisterPage from './pages/Register';
+import LoginPage from './pages/Login';
+import ReviewPage from './pages/Review';
+import NotFoundPage from './pages/NotFound';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="register" element={<RegisterPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="review/:id" element={<ReviewPage />} />
+    </Route>,
+  ),
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Layout>
+      <RouterProvider
+        router={router}
+        fallbackElement={<div>Loading....</div>}
+      />
+    </Layout>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
