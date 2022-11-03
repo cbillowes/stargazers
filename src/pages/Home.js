d6077d4d878b04e9a2e1d1dc28b68f83846c118e
Clarice Bouwer <clarice@simply.co.za>
2022-11-03 07:52:16 +0400

    Review listing and detail pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+import data from '../data/reviews.json';
+
 const HomePage = () => {
-  return <div>Home page</div>;
+  return data.map(({ id, title, description, rating }) => {
+    return (
+      <div key={id}>
+        <h2>
+          <Link to={`/review/${id}`}>{title}</Link> / {rating} ✨
+        </h2>
+        <p>{description}</p>
+      </div>
+    );
+  });
 };
 
 export default HomePage;
--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,5 +1,20 @@
+import { useParams } from 'react-router-dom';
+import NotFoundPage from './NotFound';
+import data from '../data/reviews.json';
+
 const ReviewPage = () => {
-  return <div>Review page</div>;
+  const { id } = useParams();
+  const reviews = data.filter((i) => i.id === id);
+  if (reviews.length > 0) {
+    const review = reviews[0];
+    return (
+      <div>
+        Review page <pre>{JSON.stringify(review, null, 2)}</pre>
+      </div>
+    );
+  }
+
+  return <NotFoundPage />;
 };
 
 export default ReviewPage;
