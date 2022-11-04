1579dacd510e02ab034450756df9b2976a20dc31
Clarice Bouwer <clarice@simply.co.za>
2022-11-04 06:40:39 +0400

    Fetch and display reviews on home page

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -1,14 +1,29 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data/reviews.json';
+import axios from 'axios';
 
 const HomePage = () => {
-  return data.map(({ id, title, description, rating }) => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get(`http://localhost:3001/api/reviews`);
+      setReviews(response.data);
+    };
+    fetch();
+  }, []);
+
+  if (reviews.length === 0) {
+    return <div>No results</div>
+  }
+
+  return reviews.map(({ id, title, abstract, rating }) => {
     return (
       <div key={id}>
         <h2>
           <Link to={`/review/${id}`}>{title}</Link> / {rating} ✨
         </h2>
-        <p>{description}</p>
+        <p>{abstract}</p>
       </div>
     );
   });
