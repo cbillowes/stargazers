8e8fd6008b06429449000b5be2b421f42ce170ca
Clarice Bouwer <clarice@simply.co.za>
2022-11-08 02:10:24 +0400

    Add login form

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,5 +1,68 @@
+import { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { Alert, Button, Form, TextBox } from '../components/Controls';
+
 const LoginPage = () => {
-  return <div>Login page</div>;
+  const [errorMessage, setErrorMessage] = useState('');
+  const [peeking, togglePeeking] = useState(false);
+
+  const navigate = useNavigate();
+  const email = useRef();
+  const password = useRef();
+
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(
+        getAuth(),
+        email.current.value,
+        password.current.value,
+      );
+      navigate('/profile');
+    } catch (e) {
+      setErrorMessage(e.message);
+    }
+  };
+
+  return (
+    <Form title="Login to Stargazers">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <TextBox
+          ref={email}
+          label="Email address"
+          isRequired
+          type="text"
+          placeholder="Your registered email address"
+        />
+        <TextBox
+          ref={password}
+          type={peeking ? 'text' : 'password'}
+          label={
+            <span>
+              Password{' '}
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  togglePeeking(!peeking);
+                }}
+              >
+                <span className="text-xl">{peeking ? '🫣' : '👀'}</span>
+              </button>
+            </span>
+          }
+          isRequired
+        />
+        <hr className="border-0 mt-4" />
+        <div className="flex items-center gap-4">
+          <Button onClick={login}>Login</Button>
+          <Link to="/register" className="hover:text-green-500">Need an account? Register.</Link>
+        </div>
+        {errorMessage && (
+          <Alert onClose={() => setErrorMessage('')}>{errorMessage}</Alert>
+        )}
+      </form>
+    </Form>
+  );
 };
 
 export default LoginPage;
