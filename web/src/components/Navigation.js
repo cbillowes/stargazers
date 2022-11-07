994904cded1ec635855507fa0cdb21861b76afb0
Clarice Bouwer <clarice@simply.co.za>
2022-11-08 02:11:21 +0400

    Change navigation view for logged in vs logged out user

--- web/src/components/Navigation.js
+++ web/src/components/Navigation.js
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
+import useUser from '../hooks/useUser';
 
 const Navigation = () => {
+  const { user } = useUser();
+
   return (
     <nav className="bg-blue-900 text-blue-300 py-2 px-5">
       <div className="max-w-3xl flex justify-between mx-auto">
@@ -13,12 +16,28 @@ const Navigation = () => {
         </div>
         <div>
           <ul className="flex space-x-5">
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
+            {!user && (
+              <>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </>
+            )}
+            {user && (
+              <>
+                <li>
+                  <Link to="/profile">{user.email}</Link>
+                </li>
+                <li>
+                  <button onClick={async () => await user.logout()}>
+                    Logout
+                  </button>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
